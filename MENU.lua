74d28fd4bf1eeee72841855fb78858d57c8ebc7d
PuyuBuff <113878272+PuyuBuff@users.noreply.github.com>
2022-10-05 19:08:29 +0700

    Update MENU.lua

--- MENU.lua
+++ MENU.lua
@@ -1,3 +1,24 @@
+gg.alert(os.date([[
+เวลาเปิดสคริปต์
+%c]]))
+function main()
+So = gg.choice({
+   "โอกาสโดนแบนน้อย",
+   "ฟังชั่นทั้งหมด",
+   "ออก",
+},nil,"Main MENU")
+	if So == nil then else
+	if So == 1 then mini() end --โอกาสโดนแบนน้อย
+	if So == 2 then MENU1() end --ทั้งหมด
+	if So == 3 then osexit() end
+end
+KAC=-1
+end
+
+function osexit()
+     os.exit()
+end
+
 
 function MENU1() --ทั้งหมด
 t = gg.choice({
