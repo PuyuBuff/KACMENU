1bc9b588a582d46fb3c1f922f3b535f5c96de480
PuyuBuff <113878272+PuyuBuff@users.noreply.github.com>
2022-10-05 18:51:43 +0700

    Update lobby.lua

--- lobby.lua
+++ lobby.lua
@@ -1,1 +1,26 @@
+gg.alert(os.date([[
+เวลาเปิดสคริปต์
+%c]]))
+function main()
+So = gg.choice({
+   "โอกาสโดนแบนน้อย",
+   "ฟังชั่นทั้งหมด",
+   "ออก",
+},nil,"Main MENU")
+	if So == nil then else
+	if So == 1 then mini() end --โอกาสโดนแบนน้อย
+	if So == 2 then MENU1() end --ทั้งหมด
+	if So == 3 then osexit() end
+end
+KAC=-1
+end
 
+while true do
+if gg.isVisible(true) then
+KAC=1
+gg.setVisible(false)
+end
+if KAC==1 then main()
+KAC=-1
+end
+end
