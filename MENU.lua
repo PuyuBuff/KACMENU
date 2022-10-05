1a0b721f214565ed3ed3bcf13b76ebb1e79bd052
PuyuBuff <113878272+PuyuBuff@users.noreply.github.com>
2022-10-05 19:52:31 +0700

    Update MENU.lua

--- MENU.lua
+++ MENU.lua
@@ -10,7 +10,7 @@ So = gg.choice({
 if So == nil then else
 if So == 1 then mini() end --โอกาสโดนแบนน้อย
 if So == 2 then MENU1() end --ทั้งหมด
-if So == 4 then osexit() end
+if So == 4 then os.exit() end
 end
 NUX=-1
 end
